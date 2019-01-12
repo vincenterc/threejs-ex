c7ff5d9f57a05c33a8346848562276b2b2e1c6d2
vincenterr <chung.menghsun@gmail.com>
2019-01-12 20:06:59 +0800

    First ex of three.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import * as THREE from 'three'
 import PageWrapper from '../components/PageWrapper'
 
 class HomePage extends React.Component {
+  componentDidMount() {
+    this._initThree()
+  }
+
   render() {
-    return <div>Gatsby starter!</div>
+    return <div id="three-root" />
+  }
+
+  _initThree = () => {
+    let scene = new THREE.Scene()
+    let camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+
+    let renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.querySelector('#three-root').appendChild(renderer.domElement)
+
+    let geometry = new THREE.BoxGeometry(1, 1, 1)
+    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    let cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+    camera.position.z = 5
+
+    function animate() {
+      requestAnimationFrame(animate)
+
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+
+      renderer.render(scene, camera)
+    }
+    animate()
   }
 }
 
